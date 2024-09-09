944af43f3028f922ab513d811326989ad6f68c4b
raghav0807 <raghav.agiwal2002@gmail.com>
2024-09-10 01:13:01 +0530

    Using CSS Style Sheet

--- src/components/InlineComponent.jsx
+++ src/components/InlineComponent.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-const header = { color: "blue", fontSize: "140px" };
-
+// const header = { color: "blue", fontSize: "140px" };
+import "../css/style.css";
 export default function InlineComponent() {
   return (
     <div>
-      <h1 style={header}>Inline Component</h1>
+      {/* <h1 style={header}>Inline Component</h1>
+       */}
+      <h1 className="header ">Inline Component</h1>
     </div>
   );
 }
